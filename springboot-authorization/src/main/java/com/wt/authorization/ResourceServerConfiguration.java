ff4701efe5b7988e221a40e9ddfd3ccaeeb67b5c
wutong <wt01278@163.com>
2019-04-24 20:39:46 +0800

    springboot-oauth

--- springboot-authorization/src/main/java/com/wt/authorization/ResourceServerConfiguration.java
+++ springboot-authorization/src/main/java/com/wt/authorization/ResourceServerConfiguration.java
@@ -12,15 +12,28 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
  * @date 2019-04-19 下午 6:54
  * PROJECT_NAME springboot-parent
  */
+
+/**
+ * @EnableResourceServer 注解自动增加了一个类型为 OAuth2AuthenticationProcessingFilter 的过滤器链
+ */
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfiguration implements ResourceServerConfigurer {
 
 
-
+    /**
+     * tokenServices：ResourceServerTokenServices 类的实例，用来实现令牌服务。
+     resourceId：这个资源服务的ID，这个属性是可选的，但是推荐设置并在授权服务中进行验证。
+     其他的拓展属性例如 tokenExtractor 令牌提取器用来提取请求中的令牌。
+     请求匹配器，用来设置需要进行保护的资源路径，默认的情况下是受保护资源服务的全部路径。
+     受保护资源的访问规则，默认的规则是简单的身份验证（plain authenticated）。
+     其他的自定义权限保护规则通过 HttpSecurity 来进行配置。
+     */
     @Override
     public void configure(ResourceServerSecurityConfigurer resource) throws Exception {
         resource.resourceId("order").stateless(true);
+
+
     }
 
     @Override
