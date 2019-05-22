9c406f098301e7c305034a49ffc1f07c26874ef1
wutong <wt01278@163.com>
2019-05-22 18:30:54 +0800

    springboot-oauth

--- springboot-web/src/main/java/com/wt/springboot/executor/ThreadNotify.java
+++ springboot-web/src/main/java/com/wt/springboot/executor/ThreadNotify.java
@@ -40,20 +40,19 @@ public class ThreadNotify {
         final String[] nos = ThreadHelper.buildNoArr();
         @Override
         public void run() {
-           a: for(int i=0;i<nos.length;i+=2){
+           for(int i=0;i<nos.length;i+=2){
                     synchronized (datas){
                         while(datas.tag){
                             try {
                                 datas.wait();
-
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
-                                break a;
+                                break;
                             }
                         }
                         System.out.print(nos[i]+nos[i+1]);
                         datas.tag=true;
-                        datas.notify();
+                        datas.notifyAll();
                     }
                 }
         }
@@ -63,8 +62,7 @@ public class ThreadNotify {
         String[] chars = ThreadHelper.buildCharArr();
         @Override
         public void run() {
-
-            b: for(int i=0;i<chars.length;i++){
+            for(int i=0;i<chars.length;i++){
                 synchronized (datas){
                     while(!datas.tag){
                         try {
@@ -72,12 +70,12 @@ public class ThreadNotify {
 
                         } catch (InterruptedException e) {
                             e.printStackTrace();
-                            break b;
+                            break ;
                         }
                     }
                     System.out.print(chars[i]);
                     datas.tag=false;
-                    datas.notify();
+                    datas.notifyAll();
                 }
             }
         }
@@ -85,12 +83,63 @@ public class ThreadNotify {
 
     public static void main(String[] args) throws InterruptedException {
 //        System.out.println(Arrays.toString(ThreadNotify.ThreadHelper.buildCharArr()));
-
         ExecutorService executorService = Executors.newFixedThreadPool(2);
-        executorService.submit(new ThreadNotify().new B());
-        executorService.submit(new ThreadNotify().new A());
-        executorService.shutdown();
+//        executorService.submit(new ThreadNotify().new A());
+//        executorService.submit(new ThreadNotify().new B());
+//        executorService.shutdown();
 //        https://blog.csdn.net/u011514810/article/details/77131296
+
+        executorService.submit(new ThreadNotify().newThreadOne());
+        executorService.submit(new ThreadNotify().newThreadTwo());
+        executorService.shutdown();
+
     }
 
+
+    private final ThreadToGo threadToGo = new ThreadToGo();
+    public Runnable newThreadOne() {
+        final String[] inputArr = ThreadHelper.buildNoArr();
+        return new Runnable() {
+            private String[] arr = inputArr;
+            public void run() {
+                try {
+                    for (int i = 0; i < arr.length; i=i+2) {
+                        synchronized (threadToGo) {
+                            while (threadToGo.value == 2)
+                                threadToGo.wait();
+                            System.out.print(arr[i]+arr[i + 1]);
+                            threadToGo.value = 2;
+                            threadToGo.notify();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("Oops...");
+                }
+            }
+        };
+    }
+    public Runnable newThreadTwo() {
+        final String[] inputArr = ThreadHelper.buildCharArr();
+        return new Runnable() {
+            private String[] arr = inputArr;
+            public void run() {
+                try {
+                    for (int i = 0; i < arr.length; i++) {
+                        synchronized (threadToGo) {
+                            while (threadToGo.value == 1)
+                                threadToGo.wait();
+                            System.out.print(arr[i]);
+                            threadToGo.value = 1;
+                            threadToGo.notify();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("Oops...");
+                }
+            }
+        };
+    }
+    class ThreadToGo {
+        int value = 1;
+    }
 }
