d462beb47856528b1f424d23e681baca896fcbf6
wutong <wt01278@163.com>
2019-05-28 14:10:10 +0800

    springboot-oauth

--- springboot-web/src/main/java/com/wt/springboot/async/AsyncController.java
+++ springboot-web/src/main/java/com/wt/springboot/async/AsyncController.java
@@ -1,11 +1,14 @@
 package com.wt.springboot.async;
 
+import com.google.common.collect.Ordering;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
 
 @Controller
@@ -60,5 +63,9 @@ public class AsyncController {
     public static void main(String[] args) throws  Exception{
        // new AsyncController().page2();
         System.out.println(URLDecoder.decode("tbbh=S2019050600001&ajly=&bsqd=%E7%A7%BB%E5%8A%A8%E7%AB%AF%E4%B8%8A%E6%8A%A5&szq=19&szz=19516&ytbmj=96825.86&zdmj=96825.86&sbr=%E8%AE%B8%E4%BF%8A&cjsj=2018-01-23&xsqk=&distance=111","UTF-8"));
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
+        integers.sort(Ordering.natural().reversed());
+        System.out.println(integers);
+
     }
 }
