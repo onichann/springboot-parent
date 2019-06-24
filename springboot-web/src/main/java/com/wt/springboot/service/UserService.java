d18d331f45dc3add80e14de36377d96c1340bf29
wutong <wt01278@163.com>
2019-06-24 12:02:24 +0800

    springboot-oauth

--- springboot-web/src/main/java/com/wt/springboot/service/UserService.java
+++ springboot-web/src/main/java/com/wt/springboot/service/UserService.java
@@ -5,11 +5,20 @@ import com.wt.springboot.mybatis.mapper.UserDao;
 import com.wt.springboot.pojo.User_Mybatis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.ServletContext;
 import java.util.List;
 
 @Service
 public class UserService {
+
+	@Autowired
+	private ServletContext servletContext;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
 	
 	// 注入UserRepository
 	@Autowired
@@ -30,6 +39,8 @@ public class UserService {
 	public void insertGetKey(User_Mybatis user) {
 		// 数据插入成功以后，Mybatis框架会将插入成功的数据主键存入到user对象中去
 		userRepository.insertGetKey(user);
+		WebApplicationContext currentWebApplicationContext = ContextLoader.getCurrentWebApplicationContext();
+		ServletContext servletContext = currentWebApplicationContext.getServletContext();
 	}
 	
 	public void update(User_Mybatis user) {
