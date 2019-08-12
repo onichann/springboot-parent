292ce8cf64263a13fd411e9de0d075e614f702d2
wutong <wt01278@163.comm>
2019-08-12 23:08:04 +0800

    springboot-web

---
+++ springboot-web/src/main/java/com/wt/springboot/pojo/MyUser.java
@@ -0,0 +1,15 @@
+package com.wt.springboot.pojo;
+
+/**
+ * @author wutong
+ * @date 2019/8/12 22:54
+ * PROJECT_NAME springboot-parent
+ */
+public class MyUser {
+    private int id;
+    private String loginName;
+    private String password;
+    private String userName;
+
+
+}
