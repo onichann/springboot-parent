a1b0b2464b15a7952253efa6ef6c49b02b9751f6
wutong <wt01278@163.com>
2019-10-24 18:00:21 +0800

    oldWeb

---
+++ springboot-resource/src/main/java/com/wt/resource/SpringbootResourceApplication.java
@@ -0,0 +1,13 @@
+package com.wt.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootResourceApplication.class, args);
+    }
+
+}
---
+++ springboot-resource/src/test/java/com/wt/resource/SpringbootResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wt.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
