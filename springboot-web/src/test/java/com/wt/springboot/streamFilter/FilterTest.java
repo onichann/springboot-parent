e2b27812c782b0bbe6f27a9196ab0a52579a3dfb
wutong <wt01278@163.com>
2019-11-05 15:31:27 +0800

    20191105

---
+++ springboot-web/src/test/java/com/wt/springboot/streamFilter/FilterTest.java
@@ -0,0 +1,37 @@
+package com.wt.springboot.streamFilter;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class FilterTest {
+
+    @Test
+    public void filterTest(){
+        List<Person> persons = new ArrayList<>();
+        List<Integer> ids = new ArrayList<>();//用来临时存储person的id
+        persons.add(new Person(1, "name1", 10));
+        persons.add(new Person(2, "name2", 21));
+        persons.add(new Person(5, "name5", 55));
+        persons.add(new Person(3, "name3", 34));
+        persons.add(new Person(1, "name1", 10));
+
+        List<Person> personList = persons.stream().filter(// 过滤去重
+                v -> {
+                    boolean flag = !ids.contains(v.getId());
+                    if(flag) ids.add(v.getId());
+                    return flag;
+                }
+        ).sorted(Comparator.comparingInt(Person::getId)).collect(Collectors.toList());
+        System.out.println(personList);
+        System.out.println(ids);
+        List<Person> unique = persons.stream().collect(
+                Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Person::getId))),
+                        ArrayList::new));
+        System.out.println(unique);
+    }
+}
---
+++ springboot-web/src/test/java/com/wt/springboot/streamFilter/Person.java
@@ -0,0 +1,46 @@
+package com.wt.springboot.streamFilter;
+
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
