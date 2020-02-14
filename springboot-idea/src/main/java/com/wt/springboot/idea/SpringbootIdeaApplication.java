b890ec17262e602c0249faa33f8df4663d10809a
wutong <wt01278@163.com>
2020-02-14 11:59:33 +0800

    编程思想study

---
+++ springboot-idea/src/main/java/com/wt/springboot/idea/SpringbootIdeaApplication.java
@@ -0,0 +1,14 @@
+package com.wt.springboot.idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootIdeaApplication.class, args);
+
+    }
+
+}
