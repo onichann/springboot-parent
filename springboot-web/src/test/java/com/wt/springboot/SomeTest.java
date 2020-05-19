399c6c916128237c4a31f7190a1b8d0ad20b9610
wutong <wt01278@163.com>
2020-05-19 20:48:55 +0800

    编程思想study

--- springboot-web/src/test/java/com/wt/springboot/SomeTest.java
+++ springboot-web/src/test/java/com/wt/springboot/SomeTest.java
@@ -1,7 +1,13 @@
 package com.wt.springboot;
 
 
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 /**
  * @author Administrator
@@ -12,4 +18,21 @@ public class SomeTest {
     @NotNull String foo(int x, @NotNull String data){
        return x > 0 ? data : "";
    }
+
+    public static void main(String[] args) {
+        StringBuilder result = new StringBuilder();
+        List messages = Arrays.asList("a", "b", "c");
+        List<CompletableFuture> futures = (List<CompletableFuture>) messages.stream()
+                .map(msg -> CompletableFuture.completedFuture(msg).thenApply(s -> StringUtils.upperCase((String) s)))
+                .collect(Collectors.toList());
+        CompletableFuture allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]))
+                .whenComplete((v, th) -> {
+                    result.append("done");
+                    futures.forEach(v1-> System.out.println(v1.getNow(null)));
+                });
+        //allOf.join();
+        System.out.println("done");
+
+        CompletableFuture.completedFuture("ss").thenApplyAsync(String::toUpperCase).whenComplete((v,e)-> System.out.println(v));
+    }
 }
