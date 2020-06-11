360c2543f9139aab2e61191b17382b029ab8c929
wutong <wt01278@163.com>
2020-06-11 11:38:30 +0800

    life is short

---
+++ springboot-web/src/main/java/com/wt/Mutex.java
@@ -0,0 +1,102 @@
+package com.wt;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author Administrator
+ * @date 2020-06-11 上午 9:42
+ * introduction
+ */
+public class Mutex implements Lock, java.io.Serializable{
+
+    private static final long serialVersionUID = 4217929661900056266L;
+
+    private final Sync sync = new Sync();
+
+    //0 解锁 1 锁定
+    private static class Sync extends AbstractQueuedSynchronizer {
+
+        private static final long serialVersionUID = 5478349358469259047L;
+
+        @Override
+        protected boolean tryAcquire(int acquires) {
+            assert acquires == 1;
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int releases) {
+            assert releases == 1;
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState()==1;
+        }
+
+        public Condition newCondition() { return new ConditionObject(); }
+
+        private void readObject(java.io.ObjectInputStream s)
+                throws java.io.IOException, ClassNotFoundException {
+            s.defaultReadObject();
+            setState(0); // reset to unlocked state
+        }
+    }
+
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, @NotNull TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1,unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @NotNull
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    public boolean isLocked() {
+        return sync.isHeldExclusively();
+    }
+
+    public boolean hasQueuedThreads() {
+        return sync.hasQueuedThreads();
+    }
+
+
+}
---
+++ springboot-web/src/main/java/com/wt/springboot/executor/ThreadNotify21.java
@@ -0,0 +1,78 @@
+package com.wt.springboot.executor;
+
+
+import com.wt.Mutex;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author Administrator
+ * @date 2019-05-23 上午 10:35
+ * PROJECT_NAME springboot-parent
+ */
+public class ThreadNotify21 {
+    private Lock lock =new Mutex();
+    private Condition condition=lock.newCondition();
+
+    private static class Datas{
+        private boolean tag=false;
+    }
+
+    private final static Datas datas=new Datas();
+
+    class A implements Runnable{
+        final String[] nos = ThreadNotifyHelper.buildNoArr();
+        @Override
+        public void run() {
+            for(int i=0;i<nos.length;i+=2){
+                try {
+                    lock.lock();
+                    while(datas.tag){
+                        condition.await();
+                    }
+                    System.out.print(nos[i]+nos[i+1]);
+                    datas.tag=true;
+                    condition.signalAll();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+    class B implements Runnable{
+        String[] chars = ThreadNotifyHelper.buildCharArr();
+        @Override
+        public void run() {
+            for(int i=0;i<chars.length;i++){
+                try {
+                    lock.lock();
+                    while (!datas.tag){
+                        condition.await();
+                    }
+                    System.out.print(chars[i]);
+                    datas.tag=false;
+                    condition.signalAll();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        ThreadNotify21 threadNotify2 = new ThreadNotify21();
+        executorService.submit(threadNotify2.new A());
+        executorService.submit(threadNotify2.new B());
+        executorService.shutdown();
+    }
+
+}
