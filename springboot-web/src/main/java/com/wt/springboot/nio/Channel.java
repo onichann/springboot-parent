245dab6feaf661fcc93170ecb5f06df1fdb71aa4
pat.wu <pat.wu@hrpackage.com>
2021-07-23 16:38:28 +0800

    nio

--- springboot-web/src/main/java/com/wt/springboot/nio/Channel.java
+++ springboot-web/src/main/java/com/wt/springboot/nio/Channel.java
@@ -1,5 +1,9 @@
 package com.wt.springboot.nio;
 
+import lombok.SneakyThrows;
+import org.junit.Test;
+
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -18,5 +22,40 @@ public class Channel {
         buffer.flip();
         channel.write(buffer);
         channel.close();
+    }
+
+    @SneakyThrows
+    @Test
+    public void read() {
+        FileInputStream fs = new FileInputStream("d:\\channel.txt");
+        FileChannel channel = fs.getChannel();
+        int available = fs.available();
+        ByteBuffer buffer = ByteBuffer.allocate(available);
+        channel.read(buffer);
+//        buffer.flip();
+        String str = new String(buffer.array(), 0, available);
+        System.out.println(str);
+    }
+
+    @SneakyThrows
+    @Test
+    public void copy() {
+        FileInputStream fs = new FileInputStream("d:\\channel.txt");
+        FileOutputStream fos=new FileOutputStream("d:\\copy_channel.txt");
+        FileChannel readChannel = fs.getChannel();
+        FileChannel writeChannel = fos.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        while (true) {
+            buffer.clear();
+            if (readChannel.read(buffer) == -1) {
+                break;
+            }
+            buffer.flip();
+            writeChannel.write(buffer);
+        }
+        readChannel.close();
+        writeChannel.close();
+        System.out.println("复制完成!");
     }
 }
