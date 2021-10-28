8c402daf7d9dd4a596ea035f242e4333b405a770
pat.wu <pat.wu@hrpackage.com>
2021-10-28 11:19:35 +0800

    distributedLock

--- springboot-dingding/src/main/java/com/wt/springboot/dingding/SpringbootDingdingApplication.java
+++ springboot-dingding/src/main/java/com/wt/springboot/dingding/SpringbootDingdingApplication.java
@@ -2,12 +2,50 @@ package com.wt.springboot.dingding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.PropertyMapper;
+import org.springframework.util.Assert;
 
 @SpringBootApplication
 public class SpringbootDingdingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDingdingApplication.class, args);
+    }
+
+
+
+
+
+    public PropertyMapper alwaysApplyingWhenNonNull() {
+        //            @Override
+//            public <T> PropertyMapper.Source<T> apply2(PropertyMapper.Source<T> source) {
+//                return xxx(source);
+//            }
+        return this.alwaysApplying(this::xxx);
+    }
+
+
+
+
+    public <T> PropertyMapper.Source<T> xxx(PropertyMapper.Source<T> source) {
+        return source.whenNonNull();
+    }
+
+//    private <T> PropertyMapper.Source<T> whenNonNull(PropertyMapper.Source<T> source) {
+//        return source.whenNonNull();
+//    }
+
+
+
+    public PropertyMapper alwaysApplying(SourceOperator operator) {
+        Assert.notNull(operator, "Operator must not be null");
+        return null;
+    }
+
+//    @FunctionalInterface
+    public interface SourceOperator {
+        <T> PropertyMapper.Source<T> apply(PropertyMapper.Source<T> source);
+//        <T> PropertyMapper.Source<T> apply2(PropertyMapper.Source<T> source);
     }
 
 }
