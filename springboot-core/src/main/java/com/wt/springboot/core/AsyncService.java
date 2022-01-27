50d2d4d44a4c5c6d5a808f9d2792780cb80f0e9d
pat.wu <pat.wu@hrpackage.com>
2022-01-27 13:31:19 +0800

    0127

---
+++ springboot-core/src/main/java/com/wt/springboot/core/AsyncService.java
@@ -0,0 +1,9 @@
+package com.wt.springboot.core;
+
+/**
+ * @author Pat.Wu
+ * @date 2022/1/27 11:56
+ * @description
+ */
+public class AsyncService {
+}
---
+++ springboot-core/src/main/java/com/wt/springboot/core/SpringbootCoreApplication.java
@@ -0,0 +1,13 @@
+package com.wt.springboot.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCoreApplication.class, args);
+    }
+
+}
