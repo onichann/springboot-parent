0b2c8bc01311634ac7924310a1d2f0f7adc87ab0
pat.wu <pat.wu@hrpackage.com>
2022-02-16 17:47:56 +0800

    0216

--- springboot-core/src/main/java/com/wt/springboot/core/redis/RedisDistributedLock.java
+++ springboot-core/src/main/java/com/wt/springboot/core/redis/RedisDistributedLock.java
@@ -1,10 +1,17 @@
 package com.wt.springboot.core.redis;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author Pat.Wu
  * @date 2022/2/16 16:59
  * @description
+ * https://cloud.tencent.com/developer/article/1096793
+ *https://github.com/redisson/redisson/wiki/2.-%E9%85%8D%E7%BD%AE%E6%96%B9%E6%B3%95#22-%E6%96%87%E4%BB%B6%E6%96%B9%E5%BC%8F%E9%85%8D%E7%BD%AE
  */
+@Component
 public class RedisDistributedLock {
+
+
 
 }
---
+++ springboot-core/src/main/java/com/wt/springboot/core/redis/RedissonConfig.java
@@ -0,0 +1,78 @@
+package com.wt.springboot.core.redis;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+import lombok.Getter;
+import lombok.Setter;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.Codec;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ClassUtils;
+
+/**
+ * @author Pat.Wu
+ * @date 2022/2/16 17:26
+ * @description
+ */
+@ConfigurationProperties(prefix = "spring.redisson")
+@Configuration
+@Getter
+@Setter
+public class RedissonConfig {
+    private String address;
+    private int connectionMinimumIdleSize = 10;
+    private int idleConnectionTimeout=10000;
+    private int pingTimeout=1000;
+    private int connectTimeout=10000;
+    private int timeout=3000;
+    private int retryAttempts=3;
+    private int retryInterval=1500;
+    private int reconnectionTimeout=3000;
+    private int failedAttempts=3;
+    private String password = null;
+    private int subscriptionsPerConnection=5;
+    private String clientName=null;
+    private int subscriptionConnectionMinimumIdleSize = 1;
+    private int subscriptionConnectionPoolSize = 50;
+    private int connectionPoolSize = 64;
+    private int database = 0;
+    private boolean dnsMonitoring = false;
+    private int dnsMonitoringInterval = 5000;
+
+    private int thread=8; //当前处理核数量 * 2
+
+    private String codec="org.redisson.codec.JsonJacksonCodec";
+
+    @Bean(destroyMethod = "shutdown")
+    RedissonClient redisson() throws Exception {
+        Config config = new Config();
+        config.useSingleServer().setAddress(address)
+                .setConnectionMinimumIdleSize(connectionMinimumIdleSize)
+                .setConnectionPoolSize(connectionPoolSize)
+                .setDatabase(database)
+//                .setDnsMonitoring(dnsMonitoring)
+                .setDnsMonitoringInterval(dnsMonitoringInterval)
+                .setSubscriptionConnectionMinimumIdleSize(subscriptionConnectionMinimumIdleSize)
+                .setSubscriptionConnectionPoolSize(subscriptionConnectionPoolSize)
+                .setSubscriptionsPerConnection(subscriptionsPerConnection)
+                .setClientName(clientName)
+//                .setFailedAttempts(failedAttempts)
+                .setRetryAttempts(retryAttempts)
+                .setRetryInterval(retryInterval)
+//                .setReconnectionTimeout(reconnectionTimeout)
+                .setTimeout(timeout)
+                .setConnectTimeout(connectTimeout)
+                .setIdleConnectionTimeout(idleConnectionTimeout)
+//                .setPingTimeout(pingTimeout)
+                .setPassword(password);
+        Codec codec = (Codec) ClassUtils.forName(getCodec(), ClassUtils.getDefaultClassLoader()).newInstance();
+        config.setCodec(codec);
+        config.setThreads(thread);
+        config.setEventLoopGroup(new NioEventLoopGroup());
+//        config.setUseLinuxNativeEpoll(false);
+        return Redisson.create(config);
+    }
+}
